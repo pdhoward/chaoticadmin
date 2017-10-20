49a721b9dbd3d614f9597ae5fb7ecc22d1e78ea8
pdhoward <patrick.howard@hotmail.com>
2017-10-20 15:32:33 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@
 import React from 'react';
 import './App.css';
 import { jsonServerRestClient, Admin, Resource } from 'admin-on-rest';
-import { PostList } from './posts';
+import { Delete } from 'admin-on-rest';
+import { PostList, PostCreate, PostEdit } from './posts';
 import { UserList } from './users';
 
 const App = () => (
     <Admin restClient={jsonServerRestClient('http://jsonplaceholder.typicode.com')}>
-        <Resource name="posts" list={PostList} />
+         <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} remove={Delete} />
         <Resource name="users" list={UserList} />
     </Admin>
 );
--- src/posts.js
+++ src/posts.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { List, Datagrid, TextField, EmailField, ReferenceField } from 'admin-on-rest';
+import { List, Edit, Create, Datagrid, ReferenceField, TextField,
+         EditButton, DisabledInput, LongTextInput, ReferenceInput,
+         SelectInput, SimpleForm, TextInput } from 'admin-on-rest';
 
 export const PostList = (props) => (
     <List {...props}>
@@ -10,6 +12,42 @@ export const PostList = (props) => (
             </ReferenceField>
             <TextField source="title" />
             <TextField source="body" />
+            <EditButton />
         </Datagrid>
     </List>
 );
+
+const PostTitle = ({ record }) => {
+    return <span>Post {record ? `"${record.title}"` : ''}</span>;
+};
+
+export const PostEdit = (props) => (
+    <Edit title={<PostTitle />} {...props}>
+        <SimpleForm>
+            <DisabledInput source="id" />
+            <ReferenceInput label="User" source="userId" reference="users">
+                <SelectInput optionText="name" />
+            </ReferenceInput>
+            <TextInput source="title" />
+            <LongTextInput source="body" />
+        </SimpleForm>
+    </Edit>
+);
+
+export const PostCreate = (props) => (
+    <Create {...props}>
+        <SimpleForm>
+            <ReferenceInput label="User" source="userId" reference="users" allowEmpty>
+                <SelectInput optionText="name" />
+            </ReferenceInput>
+            <TextInput source="title" />
+            <LongTextInput source="body" />
+        </SimpleForm>
+    </Create>
+);
+
+//Tip: The <Edit> and the <Create> components use the same <ReferenceInput> configuration,
+// except for the allowEmpty attribute, which is required in <Create>.
+
+// The form rendered in the create and edit pages is already functional.
+// It issues POST and PUT requests to the REST API upon submission
