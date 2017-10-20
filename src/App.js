260ee978633a34dd32908596d1182d42d3daa3fe
pdhoward <patrick.howard@hotmail.com>
2017-10-20 16:01:59 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 
-import React from 'react';
+import React                          from 'react';
 import './App.css';
-import { jsonServerRestClient, Admin, Resource } from 'admin-on-rest';
-import { Delete } from 'admin-on-rest';
-import { PostList, PostCreate, PostEdit } from './posts';
-import { UserList } from './users';
+import { jsonServerRestClient,
+        Admin, Resource }             from 'admin-on-rest';
+import { Delete }                     from 'admin-on-rest';
+import { PostList, PostCreate,
+         PostEdit }                   from './posts';
+import { UserList }                   from './users';
+import PostIcon                       from 'material-ui/svg-icons/action/book';
+import UserIcon                       from 'material-ui/svg-icons/social/group';
+
 
 const App = () => (
     <Admin restClient={jsonServerRestClient('http://jsonplaceholder.typicode.com')}>
-         <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} remove={Delete} />
-        <Resource name="users" list={UserList} />
+         <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} remove={Delete} icon={PostIcon} />
+        <Resource name="users" list={UserList} icon={UserIcon} />
     </Admin>
 );
 
