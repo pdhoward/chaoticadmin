a8b6250fe279ad3b57aa92c9f7ea631ab46b727d
pdhoward <patrick.howard@hotmail.com>
2017-10-23 09:25:39 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { PostList, PostCreate,
 import { UserList }                   from './users';
 import Dashboard                      from './DashBoard'
 import authClient                     from './authClient';
+import restClient                     from './restClient';
 import PostIcon                       from 'material-ui/svg-icons/action/book';
 import UserIcon                       from 'material-ui/svg-icons/social/group';
 
@@ -21,7 +22,7 @@ const App = () => (
 );
 */
 const App = () => (
-    <Admin authClient={authClient} dashboard={Dashboard} restClient={jsonServerRestClient('http://jsonplaceholder.typicode.com')}>
+    <Admin authClient={authClient} dashboard={Dashboard} restClient={restClient}>
         <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} remove={Delete} icon={PostIcon} />
         <Resource name="users" list={UserList} icon={UserIcon} />
     </Admin>
--- src/restClient.js
+++ src/restClient.js
@@ -10,7 +10,7 @@ import {
 } from 'admin-on-rest';
 import { stringify } from 'query-string';
 
-const API_URL = 'my.api.url';
+const API_URL = 'http://jsonplaceholder.typicode.com';
 
 /**
  * @param {String} type One of the constants appearing at the top if this file, e.g. 'UPDATE'
