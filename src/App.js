c4c54f73ddd6524fae3ccb2dc37a044321437810
pdhoward <patrick.howard@hotmail.com>
2017-10-23 09:06:05 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -12,7 +12,14 @@ import authClient                     from './authClient';
 import PostIcon                       from 'material-ui/svg-icons/action/book';
 import UserIcon                       from 'material-ui/svg-icons/social/group';
 
-
+/*
+const App = () => (
+    <Admin authClient={authClient} dashboard={Dashboard} restClient={jsonServerRestClient('http://jsonplaceholder.typicode.com')}>
+        <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} remove={Delete} icon={PostIcon} />
+        <Resource name="users" list={UserList} icon={UserIcon} />
+    </Admin>
+);
+*/
 const App = () => (
     <Admin authClient={authClient} dashboard={Dashboard} restClient={jsonServerRestClient('http://jsonplaceholder.typicode.com')}>
         <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} remove={Delete} icon={PostIcon} />
