d5005f4b5bb67a54f0e70a539a653238f13f5ddf
pdhoward <patrick.howard@hotmail.com>
2017-10-24 10:31:22 -0400

    updates

--- src/restClient.js
+++ src/restClient.js
@@ -1,3 +1,5 @@
+import { stringify } from 'query-string';
+import { fetchJson } from '../util/fetch';
 import {
     GET_LIST,
     GET_ONE,
@@ -6,126 +8,141 @@ import {
     CREATE,
     UPDATE,
     DELETE,
-    fetchUtils
-} from 'admin-on-rest';
-import { stringify } from 'query-string';
-
-
-/* pattern used in microplex to fetch an array
-getAgentArray = (cb) => {
-  let agentCall = { url: config.testdb.url,
-                    headers: { "Content-Type": "application/json",
-                               "Authorization": "token" } };
-  rp(agentCall).then(function(body) {
-      return cb(body)
-    }) }
-*/
-
-const httpClient = (url, options = {}) => {
-    options.user = {
-        authenticated: true,
-        token: 'SRTRDFVESGNJYTUKTYTHRG'
-    }
-    return fetchUtils.fetchJson(url, options);
-}
-
-const API_URL = 'http://localhost:5002/admin';
+} from './types';
 
 /**
- * @param {String} type One of the constants appearing at the top of this file, e.g. 'UPDATE'
- * @param {String} resource Name of the resource to fetch, e.g. 'posts'
- * @param {Object} params The REST request params, depending on the type
- * @returns {Object} { url, options } The HTTP request parameters
+ * Maps admin-on-rest queries to a simple REST API
+ *
+ * The REST dialect is similar to the one of FakeRest
+ * @see https://github.com/marmelab/FakeRest
+ * @example
+ * GET_LIST     => GET http://my.api.url/posts?sort=['title','ASC']&range=[0, 24]
+ * GET_ONE      => GET http://my.api.url/posts/123
+ * GET_MANY     => GET http://my.api.url/posts?filter={ids:[123,456,789]}
+ * UPDATE       => PUT http://my.api.url/posts/123
+ * CREATE       => POST http://my.api.url/posts/123
+ * DELETE       => DELETE http://my.api.url/posts/123
  */
-const convertRESTRequestToHTTP = (type, resource, params) => {
-    let url = '';
-    const options = {};
-    switch (type) {
-    case GET_LIST: {
-        const { page, perPage } = params.pagination;
-        const { field, order } = params.sort;
-        const query = {
-            sort: JSON.stringify([field, order]),
-            range: JSON.stringify([(page - 1) * perPage, page * perPage - 1]),
-            filter: JSON.stringify(params.filter),
-        };
-        url = `${API_URL}/${resource}?${stringify(query)}`;
-        break;
-    }
-    case GET_ONE:
-        url = `${API_URL}/${resource}/${params.id}`;
-        break;
-    case GET_MANY: {
-        const query = {
-            filter: JSON.stringify({ id: params.ids }),
-        };
-        url = `${API_URL}/${resource}?${stringify(query)}`;
-        break;
-    }
-    case GET_MANY_REFERENCE: {
-        const { page, perPage } = params.pagination;
-        const { field, order } = params.sort;
-        const query = {
-            sort: JSON.stringify([field, order]),
-            range: JSON.stringify([(page - 1) * perPage, (page * perPage) - 1]),
-            filter: JSON.stringify({ ...params.filter, [params.target]: params.id }),
-        };
-        url = `${API_URL}/${resource}?${stringify(query)}`;
-        break;
-    }
-    case UPDATE:
-        url = `${API_URL}/${resource}/${params.id}`;
-        options.method = 'PUT';
-        options.body = JSON.stringify(params.data);
-        break;
-    case CREATE:
-        url = `${API_URL}/${resource}`;
-        options.method = 'POST';
-        options.body = JSON.stringify(params.data);
-        break;
-    case DELETE:
-        url = `${API_URL}/${resource}/${params.id}`;
-        options.method = 'DELETE';
-        break;
-    default:
-        throw new Error(`Unsupported fetch action type ${type}`);
-    }
-    return { url, options };
-};
+export default (apiUrl, httpClient = fetchJson) => {
+    /**
+     * @param {String} type One of the constants appearing at the top if this file, e.g. 'UPDATE'
+     * @param {String} resource Name of the resource to fetch, e.g. 'posts'
+     * @param {Object} params The REST request params, depending on the type
+     * @returns {Object} { url, options } The HTTP request parameters
+     */
+    const convertRESTRequestToHTTP = (type, resource, params) => {
+        let url = '';
+        const options = {};
+        switch (type) {
+            case GET_LIST: {
+                const { page, perPage } = params.pagination;
+                const { field, order } = params.sort;
+                const query = {
+                    sort: JSON.stringify([field, order]),
+                    range: JSON.stringify([
+                        (page - 1) * perPage,
+                        page * perPage - 1,
+                    ]),
+                    filter: JSON.stringify(params.filter),
+                };
+                url = `${apiUrl}/${resource}?${stringify(query)}`;
+                break;
+            }
+            case GET_ONE:
+                url = `${apiUrl}/${resource}/${params.id}`;
+                break;
+            case GET_MANY: {
+                const query = {
+                    filter: JSON.stringify({ id: params.ids }),
+                };
+                url = `${apiUrl}/${resource}?${stringify(query)}`;
+                break;
+            }
+            case GET_MANY_REFERENCE: {
+                const { page, perPage } = params.pagination;
+                const { field, order } = params.sort;
+                const query = {
+                    sort: JSON.stringify([field, order]),
+                    range: JSON.stringify([
+                        (page - 1) * perPage,
+                        page * perPage - 1,
+                    ]),
+                    filter: JSON.stringify({
+                        ...params.filter,
+                        [params.target]: params.id,
+                    }),
+                };
+                url = `${apiUrl}/${resource}?${stringify(query)}`;
+                break;
+            }
+            case UPDATE:
+                url = `${apiUrl}/${resource}/${params.id}`;
+                options.method = 'PUT';
+                options.body = JSON.stringify(params.data);
+                break;
+            case CREATE:
+                url = `${apiUrl}/${resource}`;
+                options.method = 'POST';
+                options.body = JSON.stringify(params.data);
+                break;
+            case DELETE:
+                url = `${apiUrl}/${resource}/${params.id}`;
+                options.method = 'DELETE';
+                break;
+            default:
+                throw new Error(`Unsupported fetch action type ${type}`);
+        }
+        return { url, options };
+    };
 
-/**
- * @param {Object} response HTTP response from fetch()
- * @param {String} type One of the constants appearing at the top if this file, e.g. 'UPDATE'
- * @param {String} resource Name of the resource to fetch, e.g. 'posts'
- * @param {Object} params The REST request params, depending on the type
- * @returns {Object} REST response
- */
-const convertHTTPResponseToREST = (response, type, resource, params) => {
-    const { headers, json } = response;
-    switch (type) {
-    case GET_LIST:
-        return {
-            data: json.map(x => x),
-            total: parseInt(headers.get('content-range').split('/').pop(), 10),
-        };
-    case CREATE:
-        return { data: { ...params.data, id: json.id } };
-    default:
-        return { data: json };
-    }
-};
+    /**
+     * @param {Object} response HTTP response from fetch()
+     * @param {String} type One of the constants appearing at the top if this file, e.g. 'UPDATE'
+     * @param {String} resource Name of the resource to fetch, e.g. 'posts'
+     * @param {Object} params The REST request params, depending on the type
+     * @returns {Object} REST response
+     */
+    const convertHTTPResponseToREST = (response, type, resource, params) => {
+        const { headers, json } = response;
+        switch (type) {
+            case GET_LIST:
+            case GET_MANY_REFERENCE:
+                if (!headers.has('content-range')) {
+                    throw new Error(
+                        'The Content-Range header is missing in the HTTP Response. The simple REST client expects responses for lists of resources to contain this header with the total number of results to build the pagination. If you are using CORS, did you declare Content-Range in the Access-Control-Expose-Headers header?'
+                    );
+                }
+                return {
+                    data: json,
+                    total: parseInt(
+                        headers
+                            .get('content-range')
+                            .split('/')
+                            .pop(),
+                        10
+                    ),
+                };
+            case CREATE:
+                return { data: { ...params.data, id: json.id } };
+            default:
+                return { data: json };
+        }
+    };
 
-/**
- * @param {string} type Request type, e.g GET_LIST
- * @param {string} resource Resource name, e.g. "posts"
- * @param {Object} payload Request parameters. Depends on the request type
- * @returns {Promise} the Promise for a REST response
- */
-export default (type, resource, params) => {
-    console.log("ENTERED restClient")
-    console.log(type, resource, params)
-    const { fetchJson } = fetchUtils;
-    const { url, options } = convertRESTRequestToHTTP(type, resource, params);
-    return fetchJson(url, options)
-        .then(response => convertHTTPResponseToREST(response, type, resource, params));
+    /**
+     * @param {string} type Request type, e.g GET_LIST
+     * @param {string} resource Resource name, e.g. "posts"
+     * @param {Object} payload Request parameters. Depends on the request type
+     * @returns {Promise} the Promise for a REST response
+     */
+    return (type, resource, params) => {
+        const { url, options } = convertRESTRequestToHTTP(
+            type,
+            resource,
+            params
+        );
+        return httpClient(url, options).then(response =>
+            convertHTTPResponseToREST(response, type, resource, params)
+        );
+    };
 };
