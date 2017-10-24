9c2499bc7d13b7557784b1d6771380f75b0b9163
pdhoward <patrick.howard@hotmail.com>
2017-10-24 13:56:07 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 
 import React                          from 'react';
 import './App.css';
-import { jsonServerRestClient,
-        Admin, Resource }             from 'admin-on-rest';
+import { fetchUtils,
+         Admin, Resource }            from 'admin-on-rest';
 import { Delete }                     from 'admin-on-rest';
 import { AgentList, AgentCreate,
          AgentEdit }                  from './agents';
@@ -12,19 +12,6 @@ import authClient                     from './authClient';
 import restClient                     from './restClient';
 import AgentIcon                      from 'material-ui/svg-icons/social/person';
 import UserIcon                       from 'material-ui/svg-icons/social/group';
-
-///////////////////////////////
-////////////////////////////
-import {
-    GET_LIST,
-    GET_ONE,
-    GET_MANY,
-    GET_MANY_REFERENCE,
-    CREATE,
-    UPDATE,
-    DELETE,
-    fetchUtils,
-} from 'admin-on-rest';
 
 const API_URL = 'http://localhost:5002/admin';
 
@@ -42,7 +29,5 @@ const App = () => (
         <Resource name="users" list={UserList} icon={UserIcon} />
     </Admin>
 );
-
-
 
 export default App;
